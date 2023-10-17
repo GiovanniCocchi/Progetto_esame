2bbf4ccfacd19750b0d1c117418d1af7aa311b2f
GiovanniCocchi <228001@studenti.unimore.it>
2023-10-17 16:23:30 +0200

    chat con utente passato

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def chatpage(request):
-    return render(request,"chat/chatpage.html", context={"msg":"ChatPageRoom!"})
+    username = request.user.username
+    return render(request,"chat/chatpage.html", context={"msg":"ChatPageRoom!", 'username':username})
 
 def chatroom(request, room):
     return render(request, "chat/chatpage2.html", context={"msg":room})
