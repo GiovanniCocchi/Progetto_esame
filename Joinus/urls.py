8c50631f2be7a7d2a48ba0bd583b30fc5c73db96
GiovanniCocchi <228001@studenti.unimore.it>
2023-10-18 10:58:59 +0200

    aggiunta list view candidature per i fotografi

--- Joinus/urls.py
+++ Joinus/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from Joinus.views import Caricamento_candidatura, Conferma_candidatura
+from Joinus.views import Caricamento_candidatura, Conferma_candidatura, CandidaturaListView
 
 urlpatterns = [
     path('candidatura/', Caricamento_candidatura.as_view(), name='caricamento_candidatura'),
-    path('candidatura_inviata/', Conferma_candidatura, name='Candidaturainviata')
+    path('candidatura_inviata/', Conferma_candidatura, name='Candidaturainviata'),
+    path('lista_candidature/', CandidaturaListView.as_view(), name='Candidature')
 ]
--- Joinus/views.py
+++ Joinus/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.datetime_safe import date
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
+from Joinus.models import Candidatura
 from Photoapp.forms import CaricaCandidatura
 
 
@@ -23,3 +25,8 @@ class Caricamento_candidatura(CreateView):
 
 def Conferma_candidatura(request):
     return render(request, template_name='candidatura_inviata.html')
+
+class CandidaturaListView(LoginRequiredMixin, ListView):
+    model = Candidatura
+    template_name = 'candidatura_list.html'
+    context_object_name = 'candidature'
